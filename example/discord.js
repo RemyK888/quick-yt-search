9f56a6e2720e31915b2aa61ada5e800ee7abf4b3
RemyK888 <48729866+RemyK888@users.noreply.github.com>
2020-08-30 18:35:26 +0200

    Update discord.js
    Update Discord example

--- example/discord.js
+++ example/discord.js
@@ -3,11 +3,11 @@ const Discord = require('discord.js'); // Require Discord.Js
 const ytdl = require('ytdl-core'); // Require ytdl-core
 const client = new Discord.Client({ disableMentions: 'everyone' }); // Create new Discord client
 const YoutubeSearcher = new QuickYtSearch({ // Init the YouTubeSearch system
-    YtApiKey: 'some YouTube API key', // Place your YouTube API key here
+    YtApiKey: 'Some YouTube Api key', // Place your YouTube API key here
 });
 const config = {
-    token: 'some Discord bot token', // Place your Discord bot token here
-    'prefix': '!' // Place your bot prefix here
+    token: 'Some Discord bot token', // Place your Discord bot token here
+    prefix: '!' // Place your bot prefix here
 };
 
 client.on('message', async message => { // When client receive message
@@ -15,18 +15,32 @@ client.on('message', async message => { // When client receive message
         if (message.member.voice.channel) { // Check if message author is in a voice channel
             let args = message.content.split(' ').slice(1).join(' '); // Define the args
             if (!args) { // If no args in the message
-                message.reply('No args inserted.'); // Replay with mention the message author
+                message.reply('No args inserted.'); // Reply with mention the message author
             };
-            const connection = message.member.voice.channel.join(); // Join the message author voice channel
-            if (YoutubeSearcher.isVideoUrl(args) === false) { // If it's not a YouTube video url
-                YoutubeSearcher.getVideo(args).then(video => { // Search video
-                    connection.play(ytdl(video.url, { filter: 'audioonly' })); // Play the track using ytdl-core
-                    message.reply('Now playing ' + video.url);
+            message.member.voice.channel.join() // Join the message author voice channel
+                .then(connection => {
+                    if (YoutubeSearcher.isVideoUrl(args) === false) { // If it's not a YouTube video url
+                        YoutubeSearcher.getVideo(args).then(video => { // Search video
+                            const volume = { volume: 0.5 }; // Set the volume
+                            const dispatcher = connection.play(ytdl(video.url, { filter: 'audioonly' }, volume)); // Play the track using ytdl-core
+                            message.reply('Now playing ' + video.url); // Send a message with song informations
+                            dispatcher.on("end", () => { // When the stream is finish
+                                message.reply('End of the song.');
+                                message.member.voice.channel.leave(); // Leave user voice channel
+                                dispatcher.end(); // Stop dispatcher
+                            });
+                        });
+                    } else {
+                        const volume = { volume: 0.5 }; // Set the volume
+                        const dispatcher = connection.play(ytdl(args, { filter: 'audioonly' }, volume)); // Play the track using ytdl-core
+                        message.reply('Now playing ' + args); // Send a message with song informations
+                        dispatcher.on("end", () => { // When the stream is finish
+                            message.reply('End of the song.')
+                            message.member.voice.channel.leave(); // Leave user voice channel
+                            dispatcher.end();// Stop dispatcher
+                        });
+                    };
                 });
-            } else {
-                connection.play(ytdl(args, { filter: 'audioonly' }));
-                message.reply('Now playing ' + args);
-            };
         } else {
             message.reply('You need to join a voice channel.');
         };
